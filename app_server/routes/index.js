5c1857386aa28fe790149e822a22ed8d4fc3cef3
matteodibiscotti <matthew.lowman9@gmail.com>
2017-05-15 09:47:51 -0700

    adjustments to index.js to link route to new controller

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ctrlMain.index) {
   res.render('index', { title: 'Express' });
 });
 
