f29645f06b7fa3c5142f869941f1767815bb576d
matteodibiscotti <matthew.lowman9@gmail.com>
2017-05-16 18:41:32 +0800

    bootstrap setup and app confirmed working

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', ctrlMain.index) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrlMain.index)
 
 module.exports = router;
